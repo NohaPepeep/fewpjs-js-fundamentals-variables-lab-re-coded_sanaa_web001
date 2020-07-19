8736fbdebad3e470811be2058a6dd687a1e700b9
Nuha Al-Sameai <noha.pepeep1@gmail.com>
2020-07-19 16:39:57 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 // Code your solution in this file!
 const companyName = 'Scuber';
+<<<<<<< HEAD
 let mostProfitableNeighborhood="Chelsea";
 let companyCeo="Susan Smith";
+=======
+>>>>>>> 81062374768d3cfee0d1673ca1a7729a3462938a
